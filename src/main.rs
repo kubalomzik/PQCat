102ec5a6a1d7a66524175c3ab85b6de801817a3f
kubalomzik <jakub.lomzik@gmail.com>
2024-12-05 10:22:08 +0100

    Adjust default parameter values

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::{Parser, Subcommand};
 use std::time::Instant;
 
 mod attacks;
+mod codes;
 mod utils;
 
 use attacks::{ball_collision, lee_brickell, mmt, prange, stern};
@@ -17,53 +18,66 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Prange {
-        // fails to decode with these values
-        #[arg(short, long, default_value_t = 6)]
+        #[arg(short, long, default_value_t = 7)]
         n: usize, // Codeword length (number of bits)
-        #[arg(short, long, default_value_t = 3)]
+        #[arg(short, long, default_value_t = 4)]
         k: usize, // Message length (number of bits)
-        #[arg(short, long, default_value_t = 2)]
+        #[arg(short, long, default_value_t = 1)]
         w: usize, // Weight of the error vector (number of errors)
+        #[arg(short, long, default_value = "hamming")]
+        code_type: String, // Type of code: "random", "hamming", or "goppa"
     },
     Stern {
-        #[arg(short, long, default_value_t = 6)]
+        #[arg(short, long, default_value_t = 7)]
         n: usize,
-        #[arg(short, long, default_value_t = 3)]
+        #[arg(short, long, default_value_t = 4)]
         k: usize,
-        #[arg(short, long, default_value_t = 2)]
+        #[arg(short, long, default_value_t = 1)]
         w: usize,
+        #[arg(short, long, default_value = "hamming")]
+        code_type: String,
     },
     LeeBrickell {
-        #[arg(short, long, default_value_t = 6)]
+        #[arg(short, long, default_value_t = 7)]
         n: usize,
-        #[arg(short, long, default_value_t = 3)]
+        #[arg(short, long, default_value_t = 4)]
         k: usize,
-        #[arg(short, long, default_value_t = 2)]
+        #[arg(short, long, default_value_t = 1)]
         w: usize,
+        #[arg(short, long, default_value = "hamming")]
+        code_type: String,
     },
     BallCollision {
-        // fails to decode with these values
-        #[arg(short, long, default_value_t = 6)]
+        #[arg(short, long, default_value_t = 7)]
         n: usize,
-        #[arg(short, long, default_value_t = 3)]
+        #[arg(short, long, default_value_t = 4)]
         k: usize,
-        #[arg(short, long, default_value_t = 2)]
+        #[arg(short, long, default_value_t = 1)]
         w: usize,
+        #[arg(short, long, default_value = "hamming")]
+        code_type: String,
     },
     Mmt {
         // fails to decode with these values
-        #[arg(short, long, default_value_t = 6)]
+        #[arg(short, long, default_value_t = 7)]
         n: usize,
-        #[arg(short, long, default_value_t = 3)]
+        #[arg(short, long, default_value_t = 4)]
         k: usize,
-        #[arg(short, long, default_value_t = 2)]
+        #[arg(short, long, default_value_t = 1)]
         w: usize,
+        #[arg(short, long, default_value = "hamming")]
+        code_type: String,
+        /*
+        Parameter p (partitions) ~= log_2(n)
+        - Smaller p reduces computational cost but may miss solutions
+        - Larger p increases accuracy but grows computational cost exponentially
+        */
         #[arg(short, long, default_value_t = 2)]
-        p: usize, // Parameter p (partitions)
+        p: usize,
         #[arg(long, default_value_t = 2)]
-        l1: usize, // Parameter l1
+        l1: usize, // Error split 1
         #[arg(long, default_value_t = 2)]
-        l2: usize, // Parameter l2
+        l2: usize, // Error split 2
     },
 }
 
@@ -71,35 +85,43 @@ fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Prange { n, k, w } => {
+        Commands::Prange { n, k, w, code_type } => {
             let start = Instant::now();
-            prange::run(n, k, w);
+            prange::run(n, k, w, code_type);
             let duration = start.elapsed();
-            println!("Prange's algorithm completed in {:?}", duration);
+            println!("Time {:?}", duration);
         }
-        Commands::Stern { n, k, w } => {
+        Commands::Stern { n, k, w, code_type } => {
             let start = Instant::now();
-            stern::run(n, k, w);
+            stern::run(n, k, w, code_type);
             let duration = start.elapsed();
-            println!("Stern's algorithm completed in {:?}", duration);
+            println!("Time: {:?}", duration);
         }
-        Commands::LeeBrickell { n, k, w } => {
+        Commands::LeeBrickell { n, k, w, code_type } => {
             let start = Instant::now();
-            lee_brickell::run(n, k, w);
+            lee_brickell::run(n, k, w, code_type);
             let duration = start.elapsed();
-            println!("Lee-Brickell algorithm completed in {:?}", duration);
+            println!("Time: {:?}", duration);
         }
-        Commands::BallCollision { n, k, w } => {
+        Commands::BallCollision { n, k, w, code_type } => {
             let start = Instant::now();
-            ball_collision::run(n, k, w);
+            ball_collision::run(n, k, w, code_type);
             let duration = start.elapsed();
-            println!("Ball Collision algorithm completed in {:?}", duration);
+            println!("Time: {:?}", duration);
         }
-        Commands::Mmt { n, k, w, p, l1, l2 } => {
+        Commands::Mmt {
+            n,
+            k,
+            w,
+            code_type,
+            p,
+            l1,
+            l2,
+        } => {
             let start = Instant::now();
-            mmt::run(n, k, w, p, l1, l2);
+            mmt::run(n, k, w, code_type, p, l1, l2);
             let duration = start.elapsed();
-            println!("MMT algorithm completed in {:?}", duration);
+            println!("Time: {:?}", duration);
         }
     }
 }
